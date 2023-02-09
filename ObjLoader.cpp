e41342ef9b53caec0dd8f1b4f35bc39959b9d292
Michel Oliveira <micheltdoliveira@gmail.com>
2023-02-09 12:32:31 -0300

    add method face reader

--- ObjLoader.cpp
+++ ObjLoader.cpp
@@ -7,12 +7,31 @@ void ObjLoader::loadModel(object &Model, const char *filePath){
     cout << "hello" << endl;
 }
 
-/*
-face ObjLoader::getFace(string s){
-    
+
+
+normal ObjLoader::getNormal(string s){
+    float x, y, z;
+    sscanf(s.c_str(), "vn %f %f %f", &x, &y, &z);
+    normal n(x, y, z);
+
+    return n;
 }
 
 vertice ObjLoader::getVertice(string s){
-    
+    float x, y, z;
+    sscanf(s.c_str(),"v %f %f %f", &x, &y, &z);
+    vertice v(x, y, z);
+
+    return v;
 }
-*/
+
+face ObjLoader::getFace(string s){
+    int v0, v1, v2; //vertice
+    int vt0, vt1, vt2; //vertice de textura
+    int vn0, vn1, vn2; //vertice normal
+    sscanf(s.c_str(),"f %d/%d/%d %d/%d/%d %d/%d/%d", &v0, &vt0, &vn0, &v1, &vt1, &v2, &v2, &vt2, &vn2);
+    face f(v0,vt0,vn0,v1,vt1,vn1,v2,vt2,vn2);
+
+    return f;
+}
+
--- ObjLoader.h
+++ ObjLoader.h
@@ -1,12 +1,22 @@
 #include <vector>
 #include <string>
+
 struct face{
-    int vertice[4];
-    face(int a, int b, int c, unsigned int index){
-        vertice[0] = a;
-        vertice[1] = b;
-        vertice[2] = c;
-        vertice[3] = index;
+    int v[3];
+    int vt[3];
+    int vn[3];
+    face(int _v0, int _vt0, int _vn0, int _v1, int _vt1, int _vn1, int _v2, int _vt2, int _vn2){
+        v[0] = _v0;
+        vt[0] = _vt0;
+        vn[0] = _vn0;
+
+        v[1] = _v1;
+        vt[1] = _vt1;
+        vn[1] = _vn1;
+
+        v[2] = _v2;
+        vt[2] = _vt2;
+        vn[2] = _vn2;
     }
 };
 
@@ -19,6 +29,16 @@ struct vertice{
     }
 };
 
+struct normal{
+    float x, y, z;
+    normal(float _x, float _y, float _z){
+        x = _x;
+        y = _y;
+        z = _z;
+    }
+};
+
+
 struct object{
     std::vector<face> faces;
     std::vector<vertice> vertices;
@@ -27,15 +47,13 @@ struct object{
 
 class ObjLoader{
 private:
-    /* data */
+    
 public:
     void loadModel(object &Model, const char *filePath);
-
-    /*
     vertice getVertice(std::string s);
+    normal getNormal(std::string s);
     face getFace(std::string s);
 
-    */
 };
 
 
